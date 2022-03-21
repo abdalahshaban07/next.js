c862bc7f561535b4850321830ebdec17283ee680
AbdullahShaaban <abdalahshaban@yahoo.com>
2022-03-21 19:08:32 +0200

    router section v4 Link comp Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Link from "next/link";
 const Home = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Link href="/blog">
+        <a>BLog</a>
+      </Link>
+      <Link href="/product">
+        <a>product</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,26 @@
+import Link from "next/link";
+
 const ProductList = () => {
   return (
     <>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href={"/"}>
+        <a>Home</a>
+      </Link>
+      <h1>
+        <Link href={"/product/1"}>
+          <a>Product 1</a>
+        </Link>
+      </h1>
+      <h1>
+        <Link href={"/product/2"}>
+          <a>Product 2</a>
+        </Link>
+      </h1>
+      <h1>
+        <Link href={"/product/3"} replace>
+          <a>Product 3</a>
+        </Link>
+      </h1>
     </>
   );
 };
